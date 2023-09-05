72241838197adef16bac99b845b2c6ba0bdf4bba
maabbas <mx38x@icloud.com>
2023-09-05 19:59:26 +0300

    the test file for the reverse

--- ascii/test.sh
+++ ascii/test.sh
@@ -1,66 +1,54 @@
 #!/bin/bash
 
-echo "go run . --align right something standard"
-go run . --align right something standard
+echo "go run . --reverse example00.txt"
+go run . --reverse example00.txt
 echo
 echo "---------------------------------------------------------"
 
-echo "go run . --align=right left standard"
-go run . --align=right left standard 
+go run . --output=example00.txt "Hello World"
+echo "go run . --reverse=example00.txt"
+go run . --reverse=example00.txt 
 echo
 echo "---------------------------------------------------------"
 
-echo "go run . --align=left right standard"
-go run . --align=left right standard
+go run . --output=example01.txt "123"
+echo "go run . --reverse=example01.txt"
+go run . --reverse=example01.txt 
 echo
 echo "---------------------------------------------------------"
 
-echo "go run . --align=center hello shadow"
-go run . --align=center hello shadow
+go run . --output=example02.txt "#=\["
+echo "go run . --reverse=example02.txt"
+go run . --reverse=example02.txt 
 echo
 echo "---------------------------------------------------------"
 
-echo "go run . --align=justify "1 Two 4" shadow"
-go run . --align=justify "1 Two 4" shadow
+go run . --output=example03.txt "something&234"
+echo "go run . --reverse=example03.txt"
+go run . --reverse=example03.txt 
 echo
 echo "---------------------------------------------------------"
 
-echo "go run . --align=right 23/32 standard"
-go run . --align=right 23/32 standard
+go run . --output=example04.txt "abcdefghijklmnopqrstuvwxyz"
+echo "go run . --reverse=example04.txt"
+go run . --reverse=example04.txt 
 echo
 echo "---------------------------------------------------------"
 
-echo "go run . --align=right ABCabc123 thinkertoy"
-go run . --align=right ABCabc123 thinkertoy
+go run . --output=example05.txt "\!\" #$%&'()*+,-./"
+echo "go run . --reverse=example05.txt"
+go run . --reverse=example05.txt 
 echo
 echo "---------------------------------------------------------"
 
-echo "go run .--align=center "#$%\&\"" thinkertoy"
-go run . --align=center "#$%&\"" thinkertoy
+go run . --output=example06.txt ":;{=}?@"
+echo "go run . --reverse=example06.txt"
+go run . --reverse=example06.txt 
 echo
 echo "---------------------------------------------------------"
 
-echo "go run .--align=left '23Hello World!' standard"
-go run . --align=left '23Hello World!' standard  
-echo
-echo "---------------------------------------------------------"
-
-echo "go run . --align=justify 'HELLO there HOW are YOU?!' thinkertoy"
-go run . --align=justify 'HELLO there HOW are YOU?!' thinkertoy
-echo
-echo "---------------------------------------------------------"
-
-echo "go run . --align=right "a -\> A b -\> B c -\> C" shadow"
-go run . --align=right "a -> A b -> B c -> C" shadow
-echo
-echo "---------------------------------------------------------"
-
-echo "go run . --align=right abcd shadow"
-go run . --align=right abcd shadow
-echo
-echo "---------------------------------------------------------"
-
-echo "go run .--align=center ola standard"
-go run . --align=center ola standard
+go run . --output=example07.txt "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+echo "go run . --reverse=example07.txt"
+go run . --reverse=example07.txt  
 echo
 echo "---------------------------------------------------------"
