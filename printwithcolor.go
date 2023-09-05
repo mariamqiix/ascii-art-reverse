6e298a56ff62318c4df681d95f201b674c89fbdb
Ruqaya Helal <rhelal@noreply.learn.reboot01.com>
2023-09-05 17:11:46 +0000

    Update 'printwithcolor.go'

--- printwithcolor.go
+++ printwithcolor.go
@@ -22,12 +22,12 @@ func PrintWithColor(Words [][]string, color1, Text, letter1, validation, align s
 func CheckColors(Text1, color, letter1, validation string) map[int]string {
 	indexWcolor := make(map[int]string)
 	for i := 0; i < len(Text1); i++ {
-		if i < len(Text1)-1 && Text1[i:i+len(letter1)] == letter1 && letter1 != "" {
+		if i+len(letter1) <= len(Text1) && Text1[i:i+len(letter1)] == letter1 && letter1 != "" {
 			for j := 0; j < len(letter1); j++ {
 				indexWcolor[i] = color
 			}
 			i += len(letter1) - 1
-		} else if strings.Contains(validation, "2") && i < len(Text1)-1 && Text1[i:i+len(os.Args[4])] == os.Args[4] {
+		} else if strings.Contains(validation, "2") && i+len(letter1) <= len(Text1) && Text1[i:i+len(os.Args[4])] == os.Args[4] {
 			for j := 0; j < len(os.Args[4]); j++ {
 				indexWcolor[i] = CheckColor(os.Args[3])
 			}
